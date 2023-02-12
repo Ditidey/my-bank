879758d8af237e382aa9d5be21ed84350fcf2011
Ditidey <ditidey2017@gmail.com>
2023-02-12 12:56:29 +0000

    if

--- script/withdraw.js
+++ script/withdraw.js
@@ -7,16 +7,22 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousWithdraw = document.getElementById('withdraw-show');
     const previousWithdrawShowStr = previousWithdraw.innerText;
     const previousWithdrawShow = parseFloat(previousWithdrawShowStr);
-
-    const totalWithdraw = previousWithdrawShow + withdrawInput;
-    previousWithdraw.innerText = totalWithdraw;
-    
+ 
     const balance = document.getElementById('balance');
     const balanceShowStr = balance.innerText;
     const balanceShow = parseFloat(balanceShowStr);
 
+    withdraw.value = ' ';
+
+    if(withdrawInput > balanceShow)
+    {
+        alert('Insufficient Balance. Please input less than your current balance.');
+        return;
+    }
+
+    const totalWithdraw = previousWithdrawShow + withdrawInput;
+    previousWithdraw.innerText = totalWithdraw;
+
     const totalBalance = balanceShow - withdrawInput;
     balance.innerText = totalBalance;
-
-    withdraw.value = ' ';
 })
