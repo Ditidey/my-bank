fb5d1abfc4a6605954378fdc3380151b54f578d9
Ditidey <ditidey2017@gmail.com>
2023-02-12 12:49:13 +0000

    balance

--- script/withdraw.js
+++ script/withdraw.js
@@ -1,6 +1,22 @@
 
-document.getElementById('btn-withdraw').addEventListener('click', (){
+document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdraw = document.getElementById('withdraw-input');
     const withdrawInputStr = withdraw.value;
-    const withdrawInput = parseFloat(withdrawInputStr)
+    const withdrawInput = parseFloat(withdrawInputStr);
+
+    const previousWithdraw = document.getElementById('withdraw-show');
+    const previousWithdrawShowStr = previousWithdraw.innerText;
+    const previousWithdrawShow = parseFloat(previousWithdrawShowStr);
+
+    const totalWithdraw = previousWithdrawShow + withdrawInput;
+    previousWithdraw.innerText = totalWithdraw;
+    
+    const balance = document.getElementById('balance');
+    const balanceShowStr = balance.innerText;
+    const balanceShow = parseFloat(balanceShowStr);
+
+    const totalBalance = balanceShow - withdrawInput;
+    balance.innerText = totalBalance;
+
+    withdraw.value = ' ';
 })
