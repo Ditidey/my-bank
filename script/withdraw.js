78195f99b961722d0f4480506c55782a177d61db
Ditidey <ditidey2017@gmail.com>
2023-02-12 12:33:42 +0000

    withdraw

---
+++ script/withdraw.js
@@ -0,0 +1,6 @@
+
+document.getElementById('btn-withdraw').addEventListener('click', (){
+    const withdraw = document.getElementById('withdraw-input');
+    const withdrawInputStr = withdraw.value;
+    const withdrawInput = parseFloat(withdrawInputStr)
+})
